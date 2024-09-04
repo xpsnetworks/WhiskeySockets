a579f63b11f21e76df3dc6f8cb74cd054ad77e66
AINI TECH <rafaelribeiro.sp@gmail.com>
2024-09-03 21:42:33 -0300

    Push Buttons List
    Push Buttons List

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -586,7 +586,9 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 				}
 
 				if(additionalNodes && additionalNodes.length > 0) {
-					//(stanza.content as BinaryNode[]).push(...additionalNodes)
+					if(!buttonType) {
+						(stanza.content as BinaryNode[]).push(...additionalNodes)
+					}
 				}
 
 				logger.debug({ msgId }, `sending message to ${participants.length} devices`)
